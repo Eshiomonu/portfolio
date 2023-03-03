2138a311a6f5ffaa91382bd4736c8356c6fdc8bb
kadiri sunday <kadirisunday212@gmail.com>
2023-03-03 14:21:33 +0100

    created and design the navbar components and intro component

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from '../../img/github.png'
 import LinkedIn from '../../img/linkedin.png'
 import Instagram from '../../img/instagram.png'
+import Vector1 from '../../img/Vector1.png'
+import Vector2 from '../../img/Vector2.png'
+import boy from '../../img/boy.png'
+import thumbup from '../../img/thumbup.png'
+import Crown from '../../img/crown.png'
+import glassimoji from '../../img/glassesimoji.png'
+
 
 function Intro() {
   return (
@@ -21,7 +28,11 @@ function Intro() {
           <img src={Instagram} alt=""/>
         </div>
       </div>
-      <div className='i-right'></div>
+      <div className='i-right'>
+      <img src={Vector1} alt=""/>
+          <img src={Vector2} alt=""/>
+          <img src={boy} alt=""/>
+      </div>
     </div>
   )
 }
